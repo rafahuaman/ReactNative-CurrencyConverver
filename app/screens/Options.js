5aa8e854e283de54de642d6736d9a1f9a4fcd1e1
Rafael Huaman <leafar.huaman@gmail.com>
2018-02-26 13:44:01 -0500

    Implement error handling with react-native-dropdownalert

--- app/screens/Options.js
+++ app/screens/Options.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { ScrollView, StatusBar, Platform, Linking } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { ListItem, Separator } from '../components/List';
+import { connectAlert } from '../components/Alert';
 
 const ICON_PREFIX = Platform.OS === 'ios' ? 'ios' : 'md';
 const ICON_COLOR = '#868686';
@@ -11,6 +12,7 @@ const ICON_SIZE = 23;
 class Options extends React.Component {
   static propTypes = {
     navigation: PropTypes.object,
+    alertWithType: PropTypes.func,
   };
   handleThemePress = () => {
     this.props.navigation.navigate('Themes');
@@ -18,7 +20,7 @@ class Options extends React.Component {
 
   handleSitePress = () => {
     Linking.openURL('http://fixer.io').catch(() => {
-      alert('An error ocurred');
+      this.props.alertWithType('error', 'Sorry!', 'Cannot open right now');
     });
   };
 
@@ -44,4 +46,4 @@ class Options extends React.Component {
     );
   }
 }
-export default Options;
+export default connectAlert(Options);
