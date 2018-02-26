356d83c798300afc8899685b431c210a56f301e2
Rafael Huaman <leafar.huaman@gmail.com>
2018-02-26 11:59:18 -0500

    Add navigator from Home screen to CurrencyList.

--- app/screens/CurrencyList.js
+++ app/screens/CurrencyList.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { FlatList, View, StatusBar } from 'react-native';
-
 import { ListItem, Separator } from '../components/List';
 import currencies from '../data/currencies';
 
 const TEMP_CURRENT_CURRENCY = 'CAD';
 
 class CurrencyList extends React.Component {
+  static propTypes = {
+    navigation: PropTypes.object,
+  };
   handlePress = () => {
     console.log('Press');
+    this.props.navigation.goBack(null);
   };
 
   render() {
--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { StatusBar, KeyboardAvoidingView } from 'react-native';
 import { Container } from '../components/Container';
 import { Logo } from '../components/Logo';
@@ -15,12 +16,13 @@ const TEMP_CONVERSION_RATE = 0.7974;
 const TEMP_CONVERSION_DATE = new Date();
 
 class Home extends React.Component {
+  static propTypes = { navigation: PropTypes.object };
   handlePressBaseCurrency = () => {
-    console.log('Press Base');
+    this.props.navigation.navigate('CurrencyList', { title: 'Base Currency' });
   };
 
   handlePressQuoteCurrency = () => {
-    console.log('Press Quote');
+    this.props.navigation.navigate('CurrencyList', { title: 'Quote Currency' });
   };
 
   handleTextChange = (text) => {
